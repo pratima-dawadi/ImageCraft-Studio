20e9353c597f03ccce235a0ef6efb07354b3a130
dawadi_pratima <dawadipratima72@gmail.com>
2024-06-20 08:15:46 +0545

    add crop

--- src/js/ResetSave.ts
+++ src/js/ResetSave.ts
@@ -2,7 +2,14 @@ export class ResetSave {
   private imgSrc: HTMLImageElement;
   private saveButton: HTMLButtonElement;
   private resetButton: HTMLButtonElement;
+  private cropRect: {
+    startX: number;
+    startY: number;
+    width: number;
+    height: number;
+  } | null;
 
+  private originalSrc: string;
   private originalFilters: string;
   private originalTransform: string;
 
@@ -16,6 +23,8 @@ export class ResetSave {
     this.resetButton = resetButton;
     this.originalFilters = imgSrc.style.filter;
     this.originalTransform = imgSrc.style.transform;
+    this.originalSrc = imgSrc.src;
+    this.cropRect = null;
 
     this.addEventListeners();
   }
@@ -25,10 +34,21 @@ export class ResetSave {
     this.resetButton.addEventListener("click", this.resetImage.bind(this));
   }
 
+  public setCropRect(
+    startX: number,
+    startY: number,
+    width: number,
+    height: number
+  ) {
+    this.cropRect = { startX, startY, width, height };
+  }
+
   private resetImage() {
     this.imgSrc.style.filter = this.originalFilters;
     this.imgSrc.style.transform = this.originalTransform;
     this.imgSrc.style.clipPath = "none";
+    this.imgSrc.src = this.originalSrc;
+    this.cropRect = null;
   }
 
   private saveImage() {
@@ -37,11 +57,23 @@ export class ResetSave {
 
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    canvas.width = this.imgSrc.naturalWidth;
-    canvas.height = this.imgSrc.naturalHeight;
+    const cropRect = this.cropRect;
+    if (cropRect) {
+      canvas.width = cropRect.width;
+      canvas.height = cropRect.height;
+    } else {
+      canvas.width = this.imgSrc.naturalWidth;
+      canvas.height = this.imgSrc.naturalHeight;
+    }
 
     ctx.filter = this.imgSrc.style.filter;
-    ctx.translate(canvas.width / 2, canvas.height / 2);
+
+    if (cropRect) {
+      ctx.translate(cropRect.width / 2, cropRect.height / 2);
+    } else {
+      ctx.translate(canvas.width / 2, canvas.height / 2);
+    }
+
     ctx.rotate(
       (parseFloat(
         this.imgSrc.style.transform.match(/rotate\(([^)]+)\)/)?.[1] || "0"
@@ -57,13 +89,28 @@ export class ResetSave {
         this.imgSrc.style.transform.match(/scaleY\(([^)]+)\)/)?.[1] || "1"
       )
     );
-    ctx.drawImage(
-      this.imgSrc,
-      -canvas.width / 2,
-      -canvas.height / 2,
-      canvas.width,
-      canvas.height
-    );
+
+    if (cropRect) {
+      ctx.drawImage(
+        this.imgSrc,
+        cropRect.startX,
+        cropRect.startY,
+        cropRect.width,
+        cropRect.height,
+        -cropRect.width / 2,
+        -cropRect.height / 2,
+        cropRect.width,
+        cropRect.height
+      );
+    } else {
+      ctx.drawImage(
+        this.imgSrc,
+        -canvas.width / 2,
+        -canvas.height / 2,
+        canvas.width,
+        canvas.height
+      );
+    }
 
     link.href = canvas.toDataURL();
     link.click();
--- src/js/ResizeCrop.ts
+++ src/js/ResizeCrop.ts
@@ -1,12 +1,70 @@
+import { Rectangle } from "./Shapes";
+import { ResetSave } from "./ResetSave";
+
 export class ResizeCrop {
   private imgSrc: HTMLImageElement;
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+  private isButtonClicked: boolean;
+  private isDrawing: boolean;
+  private isMoving: boolean;
+  private startX: number;
+  private startY: number;
+  private currentWidth: number;
+  private currentHeight: number;
+  private shapes: Rectangle[] = [];
+  private selectedShapeIndex: number | null;
+  private imageContainer = document.querySelector(
+    ".view-image"
+  ) as HTMLDivElement;
+
+  saveButton = document.querySelector(".save") as HTMLButtonElement;
+  resetButton = document.querySelector(".reset") as HTMLButtonElement;
+
   constructor(imgSrc: HTMLImageElement) {
     this.imgSrc = imgSrc;
-    this.CropEventListeners();
-    this.ResizeEventListeners();
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.selectedShapeIndex = null;
+    this.isButtonClicked = false;
+    this.isDrawing = false;
+    this.isMoving = false;
+    this.startX = 0;
+    this.startY = 0;
+    this.currentWidth = 0;
+    this.currentHeight = 0;
+
+    this.cropEventListener();
+    this.setupResizeEventListeners();
   }
 
-  private ResizeEventListeners() {
+  private setupCanvas() {
+    this.canvas.width = this.imageContainer.clientWidth;
+    this.canvas.height = this.imageContainer.clientHeight;
+
+    this.canvas.style.position = "absolute";
+    this.canvas.style.top = "0";
+    this.canvas.style.left = "0";
+    this.imageContainer.appendChild(this.canvas);
+  }
+
+  private setupEventListeners() {
+    this.canvas.addEventListener("mousedown", this.handleMouseDown.bind(this));
+    this.canvas.addEventListener("mousemove", this.handleMouseMove.bind(this));
+    this.canvas.addEventListener("mouseup", this.handleMouseUp.bind(this));
+  }
+
+  private cropEventListener() {
+    const cropButton = document.querySelector("#crop") as HTMLButtonElement;
+    cropButton.addEventListener("click", () => {
+      this.isButtonClicked = true;
+      this.setupCanvas();
+      this.setupEventListeners();
+      this.drawImage();
+    });
+  }
+
+  private setupResizeEventListeners() {
     const imageWidth = document.querySelector("#width") as HTMLInputElement;
     const imageHeight = document.querySelector("#height") as HTMLInputElement;
     imageWidth.value = this.imgSrc.width.toString();
@@ -17,6 +75,7 @@ export class ResizeCrop {
       imageHeight.addEventListener("input", () => this.updateSize());
     }
   }
+
   private updateSize() {
     const imageWidth = document.querySelector("#width") as HTMLInputElement;
     const imageHeight = document.querySelector("#height") as HTMLInputElement;
@@ -32,30 +91,136 @@ export class ResizeCrop {
       if (!isNaN(newHeight) && newHeight > 0) {
         this.imgSrc.height = newHeight;
       }
+      this.setupCanvas();
+      this.drawImage();
     }
   }
 
-  CropEventListeners() {
-    const cropButton = document.querySelector("#crop") as HTMLButtonElement;
-    cropButton.addEventListener("click", () => {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-      const cropWidth = 300;
-      const cropHeight = 300;
-      canvas.width = cropWidth;
-      canvas.height = cropHeight;
-      ctx.drawImage(
-        this.imgSrc,
-        0,
-        0,
-        cropWidth,
-        cropHeight,
-        0,
-        0,
-        canvas.width,
-        canvas.height
-      );
-      this.imgSrc.src = canvas.toDataURL("image/png");
-    });
+  private drawImage() {
+    this.clearCanvas();
+    this.ctx.drawImage(
+      this.imgSrc,
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height
+    );
+  }
+
+  private handleMouseDown(event: MouseEvent) {
+    const mouseX = event.offsetX;
+    const mouseY = event.offsetY;
+    this.selectedShapeIndex = this.shapes.findIndex((shape) =>
+      shape.containsPoint(mouseX, mouseY)
+    );
+
+    if (this.selectedShapeIndex !== -1) {
+      this.isMoving = true;
+      this.startX = mouseX;
+      this.startY = mouseY;
+      return;
+    }
+
+    if (this.isButtonClicked) {
+      this.isDrawing = true;
+      this.startX = mouseX;
+      this.startY = mouseY;
+    }
+  }
+
+  private handleMouseMove(event: MouseEvent) {
+    if (this.isDrawing) {
+      const currentX = event.offsetX;
+      const currentY = event.offsetY;
+      this.clearCanvas();
+      this.drawImage();
+
+      if (this.isButtonClicked) {
+        const width = currentX - this.startX;
+        const height = currentY - this.startY;
+        this.drawShape(new Rectangle(this.startX, this.startY, width, height));
+        this.currentHeight = height;
+        this.currentWidth = width;
+        console.log(
+          `Mouse move Rectangle - StartX: ${this.startX}, StartY: ${this.startY}, Width: ${width}, Height: ${height}`
+        );
+      }
+    } else if (this.isMoving && this.selectedShapeIndex !== null) {
+      const dx = event.offsetX - this.startX;
+      const dy = event.offsetY - this.startY;
+      this.startX = event.offsetX;
+      this.startY = event.offsetY;
+
+      this.shapes[this.selectedShapeIndex].move(dx, dy);
+      this.clearCanvas();
+      this.drawImage();
+    }
+  }
+
+  private handleMouseUp(event: MouseEvent) {
+    if (this.isDrawing) {
+      const currentX = event.offsetX;
+      const currentY = event.offsetY;
+
+      if (this.isButtonClicked) {
+        this.currentWidth = currentX - this.startX;
+        this.currentHeight = currentY - this.startY;
+        this.shapes.push(
+          new Rectangle(
+            this.startX,
+            this.startY,
+            this.currentWidth,
+            this.currentHeight
+          )
+        );
+        console.log(
+          `Rectangle - StartX: ${this.startX}, StartY: ${this.startY}, Width: ${this.currentWidth}, Height: ${this.currentHeight}`
+        );
+
+        const resetSaveInstance = new ResetSave(
+          this.imgSrc,
+          this.saveButton,
+          this.resetButton
+        );
+        resetSaveInstance.setCropRect(
+          this.startX,
+          this.startY,
+          this.currentWidth,
+          this.currentHeight
+        );
+
+        const tempCanvas = document.createElement("canvas");
+        const tempCtx = tempCanvas.getContext("2d") as CanvasRenderingContext2D;
+        tempCanvas.width = this.currentWidth;
+        tempCanvas.height = this.currentHeight;
+        tempCtx.drawImage(
+          this.imgSrc,
+          this.startX,
+          this.startY,
+          this.currentWidth,
+          this.currentHeight,
+          0,
+          0,
+          this.currentWidth,
+          this.currentHeight
+        );
+        this.imgSrc.src = tempCanvas.toDataURL("image/png");
+      }
+      this.isDrawing = false;
+      this.isButtonClicked = false;
+    } else if (this.isMoving) {
+      this.isMoving = false;
+      this.selectedShapeIndex = null;
+    }
+    this.clearCanvas();
+    this.drawImage();
+  }
+
+  private drawShape(shape: Rectangle) {
+    shape.draw(this.ctx);
+  }
+
+  private clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
